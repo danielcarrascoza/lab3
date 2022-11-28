e404a6b25c57afbb756a022315d6e228e7c75e80
danielcarrascoza <71916878+danielcarrascoza@users.noreply.github.com>
2022-11-28 02:29:57 -0800

    h

--- ListExamples.java
+++ ListExamples.java
@@ -6,11 +6,7 @@ interface StringChecker { boolean checkString(String s); }
 class testString implements StringChecker{
   public boolean checkString(String s) { return s.length()>4; }
 }
-
-
 class ListExamples{
-
-
   // Returns a new list that has all the elements of the input list for which
   // the StringChecker returns true, and not the elements that return false, in
   // the same order they appeared in the input list;
--- ListExamplesTest.java
+++ ListExamplesTest.java
@@ -2,13 +2,13 @@ import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
-import org.junit.Test;
-
 interface StringChecker { boolean checkString(String s); }
 
+class testString implements StringChecker{
+    public boolean checkString(String s) { return s.length()>4; }
+  }
 
 public class ListExamplesTest{
-    
     @Test
 public void testMerge(){
     List<String> list1 = Arrays.asList("pear","banana");
@@ -24,12 +24,10 @@ public void testMerge(){
        List<String> list1 =  Arrays.asList("","pear","apple","apple","apple pie");
        List<String> list2 =  Arrays.asList("apple","apple","apple pie");
 
-       ArrayList<String> correctWords = new ArrayList<String>();
-       correctWords.add("apple");
-       correctWords.add("apple pie");
-       StringChecker sc = new testString();
+       List<String> result = ListExamples.filter(list1, new testString());
+       assertEquals(list2, result);
 
-       assertArrayEquals(list2.toArray(), ListExamples.filter(list1, sc).toArray());
+
 
     }
 
